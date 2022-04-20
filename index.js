d5432f1ba479bafdeec8abb0cdcda962abf11d76
Tashax22 <103681995+Tashax22@users.noreply.github.com>
2022-04-20 18:31:56 +0100

    Header
    Started adding the header and the menu bar. I have also tried to add a Javascript promt box just to show we can do it but its not working for me yet. Please feel free to change the answer and question for the promt.

---
+++ index.js
@@ -0,0 +1,8 @@
+const HowManyYearsVegan = (answer) =>{
+    if (answer > 5) {
+        console.log(answer + "Amazing! We hope you can discover somewhere new today, please explore");
+    } else {
+        console.log(answer + "Thats great, hopefully we can show you somewhere some great new places to try");
+    }
+};
+HowManyYearsVegan()
