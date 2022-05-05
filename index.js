bf4407ba2866b65b33ae31e9dd565cc11e47ecab
Tashax22 <103681995+Tashax22@users.noreply.github.com>
2022-05-05 19:04:13 +0100

    javascript

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ let HowManyYearsVegan = (answer) => {
     const years = prompt('How many years have you been vegan?')
     console.log(years)
     if (answer > 5) {
-        console.log(answer + "Amazing! We hope you can discover somewhere new today, please explore");
+        console.log =prompt('Amazing! We hope you can discover somewhere new today, please explore');
     } else {
-        console.log(answer + "Thats great, hopefully we can show you somewhere some great new places to try");
+        console.log =prompt('Thats great, hopefully we can show you somewhere some great new places to try');
     }
 };
 HowManyYearsVegan()
